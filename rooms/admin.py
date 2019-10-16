f09e34b2d1300fffe0d72f26b58a484b026f2fbf
Hyunjung KIM <kimbregas9@gmail.com>
2019-10-16 23:54:39 +0900

    make more sexy admin panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -14,7 +14,71 @@ class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields" : ("name", "description", "country", "address", "price")}
+        ),
+        (
+            "Times",
+            {"fields" : ("check_in", "check_out", "instant_book")}
+        ),
+        (
+            "More About the Spaces",
+            {
+                "classes" : ("collapse",),
+                "fields" : ("amenities","facilities","house_rules")
+            }
+        ),
+        (
+            "Spaces",
+            {"fields" : ("guests", "beds", "bedrooms", "baths")}
+        ),
+        (
+            "Last Detatils",
+            {"fields" : ("host",)}
+        ),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    ordering = ('name', 'price', 'bedrooms')
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules", 
+        "city", 
+        "country"
+    )
+
+    search_fields = ("=city","^host__username")
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    def count_amenities(self, obj): #self <- admin class obj <-now row
+        print(obj.amenities.all())
+        return "Potato"
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
